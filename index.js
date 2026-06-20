5bb17104a0216eac2007bfddb696dbed2c1fcd26
Mellow <95871222+Mell-o@users.noreply.github.com>
2026-06-19 20:00:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 import { accordianItems } from "./data.js";
+
+const accordianItemsObj = document.querySelector(".accordian-items")
+
+function renderAccordianItems(){
+    return accordianItems.map(({question, answer}) => {
+        return `
+        <details>
+            <summary><h2>${question}</h2></summary>
+            <p>${answer}</p>
+        </details>
+        `
+    }).join("")
+}
+
+accordianItemsObj.innerHTML = renderAccordianItems()
